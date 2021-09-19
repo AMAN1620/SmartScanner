80c5df5e6e209cffc8b6c89b1aa2340d854b40a4
Aman Kumar <aman.asingh307@gmail.com>
2021-09-19 15:12:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cu/smartScanner/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cu.smartScanner
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        @JvmStatic
+        val PHOTO_REQ_CODE = 234
+        @JvmStatic
+        val EXTRA_DATA = "data"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnTakePhoto.setOnClickListener {
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePhotoIntent, PHOTO_REQ_CODE)
+
+        }
+        cameraXActivity.setOnClickListener {
+            startActivity(Intent(this,CameraActivity::class.java))
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == PHOTO_REQ_CODE){
+            val bitmap = data?.extras?.get(EXTRA_DATA) as Bitmap
+            imgThumb.setImageBitmap(bitmap)
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
